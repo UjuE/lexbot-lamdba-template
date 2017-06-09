2ca74c2d0435beb3decafc47a83d638e33bf3386
Obianuju Vivian Ezeoke <ujuezeoke@gmail.com>
2017-06-09 14:28:45 +0100

    first commit

---
+++ src/test/java/com/ujuezeoke/bot/template/LexRequestHandlerTest.java
@@ -0,0 +1,15 @@
+package com.ujuezeoke.bot.template;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Obianuju Ezeoke on 09/06/2017.
+ */
+public class LexRequestHandlerTest {
+    @Test
+    public void justPass() {
+        assertTrue(true);
+    }
+}
